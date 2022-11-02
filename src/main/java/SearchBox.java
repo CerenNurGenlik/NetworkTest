ab83504db4bca0f062f0388efd99d2a4470bf741
Ceren <107202979+CerenNurGenlik@users.noreply.github.com>
2022-11-02 19:34:27 +0300

    Update SearchBox.java

--- src/main/java/SearchBox.java
+++ src/main/java/SearchBox.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class SearchBox {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SearchBox extends BasePage {
+
+    By searchBoxLocator = By.id("search");
+    By submitButtonLocator = By.id("search-form");
+    public SearchBox(WebDriver driver) {
+        super(driver);
+    }
+
+    public void search(String text) {
+        type(searchBoxLocator , text);
+        click(submitButtonLocator);
+    }
 }
