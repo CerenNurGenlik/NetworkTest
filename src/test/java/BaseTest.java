59b3e2b65c963860c38f609ea1f9246e5d29283c
Ceren <107202979+CerenNurGenlik@users.noreply.github.com>
2022-11-02 19:35:47 +0300

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class BaseTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(TestResultLogger.class)
+public class BaseTest {
+
+    WebDriver driver ;
+
+
+    @BeforeAll
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.network.com.tr/");
+        driver.manage().window().maximize();
+
+    }
+
+    @AfterAll
+    public void tearDown(){
+        driver.quit();
+    }
 }
+
