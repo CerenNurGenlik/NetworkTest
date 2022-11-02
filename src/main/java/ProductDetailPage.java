015b76389f73b0194ace4172d61da00763f5abb1
Ceren <107202979+CerenNurGenlik@users.noreply.github.com>
2022-11-02 19:33:34 +0300

    Update ProductDetailPage.java

--- src/main/java/ProductDetailPage.java
+++ src/main/java/ProductDetailPage.java
@@ -1,2 +1,19 @@
-public class ProductDetailPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ProductDetailPage extends BasePage {
+
+    By addToBasketButtonLocator = By.id("product__button -addToCart btn -black");
+
+    public ProductDetailPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isOnProductDetailPage() {
+        return isDisplayed(addToBasketButtonLocator);
+    }
+
+    public void addToBasket() {
+        click(addToBasketButtonLocator);
+    }
 }
