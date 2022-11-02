2055388cb9540faa099f9421aa8488a8f32226f5
Ceren <107202979+CerenNurGenlik@users.noreply.github.com>
2022-11-02 19:33:57 +0300

    Update ProductPage.java

--- src/main/java/ProductPage.java
+++ src/main/java/ProductPage.java
@@ -1,2 +1,27 @@
-public class ProductPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class ProductPage extends BasePage {
+
+    By shippingOptionLocator = By.id("productList__top");
+    By productNameLocator = new By.ByCssSelector("product__content");
+
+    public ProductPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isOnProductPage() {
+        return isDisplayed(shippingOptionLocator);
+    }
+
+    public void selectProduct(int i) {
+        getAllProducts().get(i).click();
+    }
+
+    private List<WebElement> getAllProducts(){
+        return findAll(productNameLocator);
+    }
 }
