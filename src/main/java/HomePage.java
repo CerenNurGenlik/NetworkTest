41e8774396ab15c3e42e030ccb7d99adf84c1c45
Ceren <107202979+CerenNurGenlik@users.noreply.github.com>
2022-11-02 19:32:48 +0300

    Update HomePage.java

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Stack;
+
+public class HomePage extends BasePage {
+
+    SearchBox searchBox ;
+    By basketCountLocator = By.id("header__basket--count");
+    By basketContainerLocator = By.id("header__icon -shoppingBag");
+    By acceptCookiesLocator = By.id("onetrust-accept-btn-handler");
+
+    public HomePage(WebDriver driver) {
+
+        super(driver);
+        searchBox = new SearchBox(driver);
+    }
+
+    public SearchBox searchBox() {
+        return this.searchBox;
+    }
+
+    public boolean isProductCountUp() {
+        return getBasketCount() > 0 ;
+    }
+
+    public void goToCart() {
+        click(basketContainerLocator);
+
+    }
+
+    private int getBasketCount(){
+       String count = find(basketCountLocator).getText();
+       return Integer.parseInt(count);
+    }
+
+    public void acceptCookies(){
+        if(isDisplayed(acceptCookiesLocator)){
+            click(acceptCookiesLocator);
+        }
+    }
 }
