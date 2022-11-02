f60e2e3a63b18a5842af8a36472ae86ba951c7e0
Ceren <107202979+CerenNurGenlik@users.noreply.github.com>
2022-11-02 19:32:20 +0300

    Update CartPage.java

--- src/main/java/CartPage.java
+++ src/main/java/CartPage.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class CartPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class CartPage extends BasePage {
+    By productNameLocator = new By.ByCssSelector("header__basketProductDesc");
+
+
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean checkIfProductAdded() {
+        return getProducts().size() > 0 ;
+    }
+
+    private List<WebElement> getProducts(){
+        return findAll(productNameLocator);
+    }
 }
